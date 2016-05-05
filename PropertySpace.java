4905357cca14473ac931e04d8ec9a59d78aaed86
johnghantous <johnghantous@JMAC.local>
2016-05-04 17:46:03 -0700

    fixed bug with pop-up windows

--- PropertySpace.java
+++ PropertySpace.java
@@ -1,5 +1,4 @@
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
@@ -26,8 +25,12 @@ public class PropertySpace extends Space implements ActionListener
     JPanel bottom;
     JPanel buyPanel;
     JFrame buyFrame;
+    JPanel confirmContainer;
     Cornerspace[]cornerList;
     String propertyTitle;
+    JButton yes;
+    JButton cancel;
+    JButton closeInfo;
     int rent;
     public PropertySpace(Color c, int p)
     {
@@ -35,11 +38,29 @@ public class PropertySpace extends Space implements ActionListener
         this.setBorder(BorderFactory.createLineBorder(Color.black));
         topColor = new JPanel();
         topColor.setBackground(c);
-        propertyName = new JLabel(propertyTitle);
-        cost = new JLabel();
-        owner = new JLabel("Owner: " + ownerName);
+        
+        propertyName = new JLabel(propertyTitle, JLabel.CENTER);
+        
+        cost = new JLabel("", JLabel.CENTER);
+        //cost.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+        owner = new JLabel("Owner: " + ownerName, JLabel.CENTER);
+        //owner.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+        
         bottom = new JPanel();
         buy = new JButton("Buy");
+        
+        confirmContainer = new JPanel();
+        confirmContainer.setLayout(new GridLayout(1,2));
+        yes = new JButton("Yes");
+        yes.addActionListener(this);
+        cancel = new JButton("Cancel");
+        cancel.addActionListener(this);
+        confirmContainer.add(yes);
+        confirmContainer.add(cancel);
+
+        closeInfo = new JButton("Close Window");
+        closeInfo.addActionListener(this);
+
         buy.addActionListener(this);
         info = new JButton("Info");
         info.addActionListener(this);
@@ -62,31 +83,46 @@ public class PropertySpace extends Space implements ActionListener
         if (e.getSource()==buy)
         {
             buyPanel = new JPanel();
-            buyPanel.setLayout(new GridLayout(4,1));
+            buyPanel.setLayout(new GridLayout(5,1));
             buyPanel.add(topColor);
             buyPanel.add(propertyName);
             buyPanel.add(cost);
-            JLabel confirm = new JLabel("Are you sure you want to buy this property?");
+            JLabel confirm = new JLabel("Are you sure you want to buy this property?", JLabel.CENTER);
+            confirm.setFont(new Font("Lucida Grande", Font.BOLD, 15));
             buyPanel.add(confirm);
+            buyPanel.add(confirmContainer);
             buyFrame = new JFrame();
-            buyFrame.setSize(500,444);
+            buyFrame.setSize(400,500);
             buyFrame.add(buyPanel);
             buyFrame.setVisible(true);
         }
         else if (e.getSource()==info)
         {
             infoPanel = new JPanel();
-            infoPanel.setLayout(new GridLayout(3,1));
+            infoPanel.setLayout(new GridLayout(4,1));
             infoPanel.add(topColor);
             infoPanel.add(propertyName);
-
-            JLabel rentLabel = new JLabel("Rent: $" + priceList[pos]/8);
+            JLabel rentLabel = new JLabel("Rent: $" + priceList[pos]/8, JLabel.CENTER);
+            rentLabel.setFont(new Font("Lucida Grande", Font.BOLD, 15));
             infoPanel.add(rentLabel);
+            infoPanel.add(closeInfo);
+
             infoFrame = new JFrame();
             infoFrame.setSize(500,444);
-            infoFrame.add(buyPanel);
+            infoFrame.add(infoPanel);
             infoFrame.setVisible(true);
+        }
+        else if (e.getSource()==yes)
+        {
+            //invoke buy transanction
+        }
+        else if (e.getSource()==cancel)
+        {
+            buyFrame.dispatchEvent(new WindowEvent(buyFrame, WindowEvent.WINDOW_CLOSING));
+        }
+        else if (e.getSource()==closeInfo)
+        {
+            infoFrame.dispatchEvent(new WindowEvent(infoFrame, WindowEvent.WINDOW_CLOSING));
         }
     }
 }
-
